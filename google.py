33883d82c2aa71f3352558c3566eb9633ff42cc4
youtube-jocoding <adjocoding@gmail.com>
2020-10-09 20:20:49 +0900

    first commit

---
+++ google.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import urllib.request
+
+driver = webdriver.Chrome()
+driver.get("https://www.naver.com")
+elem = driver.find_element_by_name("q")
+elem.send_keys("조코딩")
+elem.send_keys(Keys.RETURN)
+
+SCROLL_PAUSE_TIME = 1
+# Get scroll height
+last_height = driver.execute_script("return document.body.scrollHeight")
+while True:
+    # Scroll down to bottom
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    # Wait to load page
+    time.sleep(SCROLL_PAUSE_TIME)
+    # Calculate new scroll height and compare with last scroll height
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        try:
+            driver.find_element_by_css_selector(".mye4qd").click()
+        except:
+            break
+    last_height = new_height
+
+images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
+count = 1
+for image in images:
+    try:
+        image.click()
+        time.sleep(2)
+        imgUrl = driver.find_element_by_xpath('/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div[1]/div[1]/div/div[2]/a/img').get_attribute("src")
+        urllib.request.urlretrieve(imgUrl, str(count) + ".jpg")
+        count = count + 1
+    except:
+        pass
+
+driver.close()
