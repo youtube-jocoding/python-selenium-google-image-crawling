690e5f5f18d6fd7a2677b042871539b128b23dff
youtube-jocoding <adjocoding@gmail.com>
2020-10-10 19:18:21 +0900

    add headers 추가

--- google.py
+++ google.py
@@ -4,7 +4,7 @@ import time
 import urllib.request
 
 driver = webdriver.Chrome()
-driver.get("https://www.naver.com")
+driver.get("https://www.google.co.kr/imghp?hl=ko&tab=wi&authuser=0&ogbl")
 elem = driver.find_element_by_name("q")
 elem.send_keys("조코딩")
 elem.send_keys(Keys.RETURN)
@@ -33,6 +33,9 @@ for image in images:
         image.click()
         time.sleep(2)
         imgUrl = driver.find_element_by_xpath('/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div[1]/div[1]/div/div[2]/a/img').get_attribute("src")
+        opener=urllib.request.build_opener()
+        opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+        urllib.request.install_opener(opener)
         urllib.request.urlretrieve(imgUrl, str(count) + ".jpg")
         count = count + 1
     except:
